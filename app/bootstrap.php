92d2c3fda85fb12a05e8f9f590ef88b38fb78189
JMSajib <jm.sajib012@gmail.com>
2018-07-25 01:12:55 +0600

    Added config file and auto loaded libraries

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,11 @@
 <?php
-  // Load Libraries
-  require_once 'libraries/core.php';
-  require_once 'libraries/controller.php';
-  require_once 'libraries/database.php';
+
+  //Load Config
+  require_once 'config/config.php';
+
+  //AutoLoad Libraries
+  spl_autoload_register(function($className){
+    require_once 'libraries/' . $className . '.php';
+  })
+
+?>  
---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+    //App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    //URL Root
+    define('URLROOT','http://localhost/core-mvc');
+    //Site Name
+    define('SITENAME', 'CORE-MVC');
+?>
